f15ff3fdf7ea785fb08f6755b064cb8f12ddd0ff
eakathit <eakarthitx@gmail.com>
2026-03-17 09:38:24 +0700

    fix: autofocus input report

--- src/components/DailyReportForm.tsx
+++ src/components/DailyReportForm.tsx
@@ -165,7 +165,6 @@ function SelectField({ label, icon, value, options, placeholder, onChange, disab
                     <circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/>
                   </svg>
                   <input
-                    autoFocus
                     type="text"
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
